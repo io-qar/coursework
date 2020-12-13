7c7c55ffa5f990695884b50ad853a67760ceb885
io+_ <40149230+ioDar65@users.noreply.github.com>
2020-12-13 21:40:48 +0300

    Update buttonup.js

--- HTMLs/buttonup.js
+++ HTMLs/buttonup.js
@@ -1,21 +1,29 @@
 jQuery(document).ready(function($){
-    $('<style>'+
-        '.scrollTop{ display:none; z-index:9999; position:fixed;'+
-            'bottom:20px; left:90%; width:88px; height:125px;'+
-            'background:url(https://biznessystem.ru/img/arrow.png) 0 0 no-repeat; }' +
+    $('<style>' +
+        '.scrollTop{display:none; z-index:9999; position:fixed;' +
+        'bottom:20px; left:90%; width:88px; height:125px;' +
+        'background:url(https://biznessystem.ru/img/arrow.png) 0 0 no-repeat; }' +
         '.scrollTop:hover{ background-position:0 -133px;}'
-    +'</style>').appendTo('body');
+    + '</style>').appendTo('body');
+
     var speed = 550,
-    $scrollTop = $('<a href="#" class="scrollTop">').appendTo('body');		
+    $scrollTop = $('<a href="#" class="scrollTop">').appendTo('body');
+
     $scrollTop.click(function(e){
         e.preventDefault();
-        $( 'html:not(:animated),body:not(:animated)' ).animate({ scrollTop: 0}, speed );
+
+        $('html:not(:animated), body:not(:animated)').animate({
+            scrollTop: 0
+        }, speed);
     });
 
-    //появление
     function show_scrollTop(){
-        ( $(window).scrollTop() > 330 ) ? $scrollTop.fadeIn(700) : $scrollTop.fadeOut(700);
+        ($(window).scrollTop() > 330 ) ? $scrollTop.fadeIn(700) : $scrollTop.fadeOut(700);
     }
-    $(window).scroll( function(){ show_scrollTop(); } );
+
+    $(window).scroll(function(){
+        show_scrollTop();
+    });
+    
     show_scrollTop();
 });
