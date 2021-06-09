92a6f04f99886fadb584ec2a34045ba95b8e6e0d
io-qar <40149230+io-qar@users.noreply.github.com>
2021-06-09 20:00:25 +0300

    edit spaces

--- HTMLs/anchor.js
+++ HTMLs/anchor.js
@@ -1,8 +1,9 @@
-$(function(){
+$(function() {
     $('a[href^="#"]').on('click', function(event) {
-        event.preventDefault(); //отменяем стандартное действие
+        event.preventDefault(); //отмена стандартного действия
       
-        var sc = $(this).attr("href"), dn = $(sc).offset().top;
+        var sc = $(this).attr("href"),
+            dn = $(sc).offset().top;
         //sc - в переменную заносим информацию о том, к какому блоку надо перейти
         //dn - определяем положение блока на странице
 
